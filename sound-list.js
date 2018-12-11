839307a9722cb97c02dfa7c42289aa7849e06d06
Steven Pescheteau <steven.pescheteau@gmail.com>
2018-12-11 23:24:17 +0100

    12 new sounds

--- sound-list.js
+++ sound-list.js
@@ -1,20 +1,68 @@
 var Path = 'sounds/';
 
-var Sounds = { 'sounds' : [   
+var Sounds = { 'sounds' : [
+  {
+    'name'  : 'Ah!',
+    'url': 'sounds/Ah.mp3',
+  }, 
+  {
+    'name'  : 'BaDumTss',
+    'url': 'sounds/BaDumTss.mp3',
+  }, 
+  {
+    'name'  : 'Chalet',
+    'url': 'sounds/Chalet.mp3',
+  }, 
+  {
+    'name'  : 'Daccord',
+    'url': 'sounds/Daccord.mp3',
+  }, 
+  {
+    'name'  : 'Cerveau',
+    'url': 'sounds/Cerveau.mp3',
+  }, 
+  {
+    'name'  : 'Enemene!',
+    'url': 'sounds/Enemenemene.mp3',
+  }, 
+  {
+    'name'  : 'Erotic',
+    'url': 'sounds/EroticMoment.mp3',
+  }, 
+  {
+    'name'  : 'Feels Alone',
+    'url': 'sounds/HelloDarkness.mp3',
+  }, 
+  {
+    'name'  : 'IAMTHEONE',
+    'url': 'sounds/IAmTheOne.mp3',
+  },
+  {
+    'name'  : 'Pas Faux',
+    'url': 'sounds/PasFaux.mp3',
+  },
+  {
+    'name'  : 'Niska',
+    'url': 'sounds/Pouloulou.mp3',
+  },
+  {
+    'name'  : 'Souffrir',
+    'url': 'sounds/Souffrir ok.mp3',
+  },
+  {
+    'name'  : 'Rolling',
+    'url': 'sounds/TheySeeMeRolling.mp3',
+  },
   {
     'name'  : 'Say!',
     'url': 'sounds/Say!.mp3',
-  },
-  {
-    'name'  : 'What?!',
-    'url': 'sounds/What.mp3',
   },
   {
     'name'  : 'Say What!',
     'url': 'sounds/SayWhat.mp3',
   },
   {
-    'name'  : 'Enemene!',
-    'url': 'sounds/Enemenemene.mp3',
-  },
+    'name'  : 'What?!',
+    'url': 'sounds/What.mp3',
+  }, 
 ]};
